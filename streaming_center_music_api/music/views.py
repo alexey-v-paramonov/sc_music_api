f8c238f4f87b2272257718f4ab2ff9f8deb885e5
Alexey V Paramonov <me@paramonov.ru>
2021-11-09 21:02:36 +0300

    Add spotipy dependency, query param for the search

--- streaming_center_music_api/music/views.py
+++ streaming_center_music_api/music/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 
 class MusicAPI(APIView):
 
@@ -8,4 +8,7 @@ class MusicAPI(APIView):
     permission_classes = ()
 
     def get(self, request):
+        q = request.query_params.get("q")
+        if not q:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response({"result": "ok"})
