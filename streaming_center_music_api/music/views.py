8cc93fbd7aa076a0c449d22a83b09cf8e6dfb326
Alexey V Paramonov <me@paramonov.ru>
2021-11-10 08:41:22 +0300

    LastFM API

--- streaming_center_music_api/music/views.py
+++ streaming_center_music_api/music/views.py
@@ -1,6 +1,6 @@
 import json
 import string
-
+import requests
 import redis
 import spotipy
 from django.conf import settings
@@ -24,6 +24,14 @@ class MusicAPI(APIView):
             or track_info.get("large_image")
         )
 
+    def is_everything_found(self, track_info):
+        return bool(
+            track_info.get("isrc")
+            and track_info.get("small_image")
+            and track_info.get("medium_image")
+            and track_info.get("large_image")
+        )
+
     def get(self, request):
         q = request.query_params.get("q", "").strip()
         if not q or len(q) < 5:
@@ -31,9 +39,9 @@ class MusicAPI(APIView):
 
         key = q.translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
         r = redis.Redis(host="localhost", port=6379, db=0)
-        cached_track_info = r.get(key)
-        if cached_track_info:
-            return Response(json.loads(cached_track_info))
+        #cached_track_info = r.get(key)
+        #if cached_track_info:
+        #    return Response(json.loads(cached_track_info))
 
         track_info = {}
         client_credentials_manager = SpotifyClientCredentials(
@@ -44,11 +52,12 @@ class MusicAPI(APIView):
         spotify = spotipy.Spotify(
             client_credentials_manager=client_credentials_manager, requests_timeout=2
         )
-        try:
-            results = spotify.search(q=q, limit=1, type="track")
-        except Exception as e:
-            print(e)
-            pass
+        results = None
+        # try:
+        #     results = spotify.search(q=q, limit=1, type="track")
+        # except Exception as e:
+        #     print(e)
+        #     pass
 
         if results and results["tracks"]["items"]:
             track = results["tracks"]["items"][0]
@@ -59,6 +68,25 @@ class MusicAPI(APIView):
                 track_info["small_image"] = album["images"][-1]["url"]
                 track_info["medium_image"] = album["images"][-2]["url"]
                 track_info["large_image"] = album["images"][-3]["url"]
+
+        # LastFM
+        API_BASE = "https://ws.audioscrobbler.com/2.0/";
+        artist_q = "metallica"
+        track_q = "battery"
+        url = f"{API_BASE}?artist={artist_q}&track={track_q}&method=track.getInfo&api_key={settings.LASTFM_API_KEY}&format=json"
+        try:
+            response = requests.get(url, timeout=2)
+        except:
+            response = None
+
+        if response and response.ok:
+            j = response.json()
+            track_info["track_mbid"] = j.get('track', {}).get('mbid')
+            images = j.get('track', {}).get('album', {}).get('image')
+            if len(images) > 2:
+                track_info["small_image"] = images[-3]["#text"]
+                track_info["medium_image"] = images[-2]["#text"]
+                track_info["large_image"] = images[-1]["#text"]
 
         # Store into the cache if something was found
         if self.is_successful_search(track_info):
