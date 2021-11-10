1f09a4dc09fbc5e995ee71f84241d82ba6b434cf
Alexey V Paramonov <me@paramonov.ru>
2021-11-10 08:07:12 +0300

    Add Redis cache

--- streaming_center_music_api/music/views.py
+++ streaming_center_music_api/music/views.py
@@ -1,13 +1,15 @@
+import json
 import string
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
+import redis
 import spotipy
-from spotipy.cache_handler import CacheFileHandler
-import sys
-from spotipy.oauth2 import SpotifyClientCredentials
 from django.conf import settings
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from spotipy.cache_handler import CacheFileHandler
+from spotipy.oauth2 import SpotifyClientCredentials
+
 
 class MusicAPI(APIView):
 
@@ -16,45 +18,49 @@ class MusicAPI(APIView):
 
     def is_successful_search(self, track_info):
         return bool(
-            track_info.get("isrc") or track_info.get('small_image') or track_info.get('medium_image') or track_info.get('large_image')
+            track_info.get("isrc")
+            or track_info.get("small_image")
+            or track_info.get("medium_image")
+            or track_info.get("large_image")
         )
 
     def get(self, request):
         q = request.query_params.get("q", "").strip()
-        if not q:
+        if not q or len(q) < 5:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        key = q.translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
+        r = redis.Redis(host="localhost", port=6379, db=0)
+        cached_track_info = r.get(key)
+        if cached_track_info:
+            return Response(json.loads(cached_track_info))
 
         track_info = {}
         client_credentials_manager = SpotifyClientCredentials(
             settings.SPOTIFY_CLIENT_ID,
             settings.SPOTIFY_CLIENT_SECRET,
-            cache_handler=CacheFileHandler(cache_path="/tmp/spotify_token")
+            cache_handler=CacheFileHandler(cache_path="/tmp/spotify_token"),
         )
         spotify = spotipy.Spotify(
-            client_credentials_manager=client_credentials_manager,
-            requests_timeout=2
+            client_credentials_manager=client_credentials_manager, requests_timeout=2
         )
         try:
-            results = spotify.search(
-                q=q,
-                limit=1,
-                type='track'
-            )
+            results = spotify.search(q=q, limit=1, type="track")
         except Exception as e:
             print(e)
             pass
 
-        if results and results['tracks']['items']:
-            track = results['tracks']['items'][0]
-            ext_ids = track.get('external_ids', {})
-            track_info['isrc'] = ext_ids.get('isrc', None)
-            album = track.get('album', {})
-            if album.get('images', []) and len(album['images']) >= 3:
-                track_info['small_image'] = album['images'][-1]['url']
-                track_info['medium_image'] = album['images'][-2]['url']
-                track_info['large_image'] = album['images'][-3]['url']
+        if results and results["tracks"]["items"]:
+            track = results["tracks"]["items"][0]
+            ext_ids = track.get("external_ids", {})
+            track_info["isrc"] = ext_ids.get("isrc", None)
+            album = track.get("album", {})
+            if album.get("images", []) and len(album["images"]) >= 3:
+                track_info["small_image"] = album["images"][-1]["url"]
+                track_info["medium_image"] = album["images"][-2]["url"]
+                track_info["large_image"] = album["images"][-3]["url"]
 
         # Store into the cache if something was found
         if self.is_successful_search(track_info):
-            key = q.translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
+            r.set(key, json.dumps(track_info), ex=settings.TRACK_INFO_EXPIRE_SECONDS)
         return Response(track_info)
